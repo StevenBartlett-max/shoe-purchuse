6fa6e42416738344d9d40adbf4f05ee81fb06702
Nuredin Yesuf <61741235+NuredinYesuf@users.noreply.github.com>
2025-03-01 16:21:46 +0300

    final
    final

---
+++ app/profile/page.tsx
@@ -0,0 +1,10 @@
+import ProfileContent from "../components/ProfileContent"
+
+export default function ProfilePage() {
+  return (
+    <div className="min-h-screen bg-background">
+      <ProfileContent />
+    </div>
+  )
+}
+
